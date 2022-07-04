d928dc9e17934edc10afa120d25db51d37fdcc04
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-04 07:23:24 -0700

    printing with puts command in c and / for new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Decription: 'printing with puts'
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
