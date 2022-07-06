3e09f54ff889b3bab66e7542b278b7defb1217db
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-06 16:01:45 -0700

    learning to print random integers in c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /**
- * main -enrty point
- * more headers goes there
- * Return: 'always 0'
- * description: this prints random integers
+ *main : entry point
+ *
+ *Decription: this program prints random integers
+ *
+ *decription: random numbers 
+ *
+ *return: always 0
  */
 int main(void)
 {
-		int n;
-
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-		/* your code goes there */
-		return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+return (0);
 }
