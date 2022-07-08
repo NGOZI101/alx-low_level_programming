9f6248daac8ec9ed74137ab6e021e916ef362c6d
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-08 08:21:44 -0700

    Write a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is twhere the program begins
+ *
+ * description: this teaches how to print using putchar and ascii code
+ *
+ * Return: alwasy 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 97 ; ch <= 122 ; ch++)
+	putchar(ch);
+	putchar(\n);
+return (0);
+}
