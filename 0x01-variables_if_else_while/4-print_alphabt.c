047b275717f59c40144d25f2bf32ccf1eafb2dc7
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-09 06:33:36 -0700

    a program that prints the alphabet in lowercase, followed by a new line.
        Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 int ch;
-for (ch = 97; ch = 122 ; ch++)
+for (ch = 97; ch <= 122 ; ch++)
 {
-for (ch != 101 ; ch != 113 ;)
+if (ch != 101 && ch != 113)
 	putchar(ch);
-putchar('\n');
 }
+putchar('\n');
 return (0);
 }
