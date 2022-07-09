1a3d3560053eef4c9e56b3d9ce7bade92d4406b4
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-09 06:49:51 -0700

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the functio that begins the program
+ *
+ * this code prints integers from 0 to 9
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int n;
+for (n = 0 ; n <= 9 ; n++)
+	printf("%d", n);
+printf("\n");
+return (0);
+}
