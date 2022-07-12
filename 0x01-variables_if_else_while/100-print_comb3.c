1b59ab9250c3d4179e43ec831e6d8e988062690f
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-12 12:10:29 -0700

    combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * printind double digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+int n;
+for (ch = 48; ch <= 57; ch++)
+{
+	for (n = 49; n <= 57; n++)
+	{
+		if (n > ch)
+	{
+		putchar(ch);
+		putchar(n);
+	}
+		if (ch != 56 && n != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+}
+}
+
+						
+	
