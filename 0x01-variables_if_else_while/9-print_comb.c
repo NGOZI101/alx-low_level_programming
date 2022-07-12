dc87106b7d3af84ff8e67579cc1172340c79733f
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-12 07:45:28 -0700

    nnbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-	int ch;
-	for (ch = 0 ; ch < 10 ; ch++)
+int ch;
+for (ch = 0 ; ch < 10 ; ch++)
+{
+	putchar(ch +'0');
+	if (ch != 90);
 	{
-		putchar(ch +'0');
-		if (ch != 90);
-		{ 
-			putchar(44);
-			putchar(32);
-		}
-
+		putchar(44);
+		putchar(32);
 	}
-	putchar('\n');
+
+}
+putchar('\n');
 return (0);
 }
