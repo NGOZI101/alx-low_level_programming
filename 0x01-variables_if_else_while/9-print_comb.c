183a304aa11d2b664cd26cfe6c9f336d862ccfe4
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-12 07:37:07 -0700

    numbers with space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int ch;
+	for (ch = 0 ; ch < 10 ; ch++)
+	{
+		putchar(ch +'0');
+		if (ch != 90);
+		{ 
+			putchar(44);
+			putchar(32);
+		}
+
+	}
+	putchar('\n');
+return (0);
+}
