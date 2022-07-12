cf0f55f44217dd5edc3ac80319692a48f9beaa49
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-12 11:10:19 -0700

    numbers seperated by comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,12 +5,12 @@ int main(void)
 int ch;
 for (ch = 0 ; ch < 10 ; ch++)
 {
-	putchar(ch +'0');
-	if (ch != 90);
-	{
-		putchar(44);
-		putchar(32);
-	}
+putchar(ch + '0');
+if (ch != 90)
+{
+	putchar(44);
+	putchar(32);
+}
 
 }
 putchar('\n');
