98486bc2c3f5eb1fc052cd8eafdb0375e12745ed
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-12 11:19:17 -0700

    numbers with comma and space using putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main- entry point
+ *
+ * this prints the numbers with space inbetween and commas using putchar()
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
@@ -6,13 +13,13 @@ int ch;
 for (ch = 0 ; ch < 10 ; ch++)
 {
 putchar(ch + '0');
-if (ch != 90)
+if (ch < 9)
 {
 	putchar(44);
 	putchar(32);
 }
-
+if (ch == 9)
+	putchar('\n');
 }
-putchar('\n');
 return (0);
 }
