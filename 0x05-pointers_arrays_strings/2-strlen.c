4c5b6f5412f62d9874fe1c9c49854b07ba150a1f
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-12 20:40:39 -0700

    a nfunction that returns lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * _strlen -> this is used to calculate lenght of string
+ * @s: string pointer to passed to this functio
+ * Return: returns the lenght of string
+ */
+int _strlen(char *s)
+{
+int len;
+
+while (*s != '\0')
+{
+len += 1;
+*s = *s + 1;
+}
+return (len);
+}
