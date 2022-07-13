7342b2dcb081bacedbf6e52da40bff211304121d
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-12 21:06:42 -0700

    printing the length of string

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,7 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
+#include <stdio.h>
+#include < stdlib.h>
+#include <string>
+
+//#ifndef MAIN_H
+//#define MAIN_H
+/*int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 
-#endif
+#endif*/
+int main ( )
+{
+	char *x = "Hello World";
+	char *s = &x;
+
+int len;
+	for (; *s != '\0'; s++)
+	{
+		len += 1;
+		s++;
+	}
+	printf("%d\n", len);
+	return (0);
+}
