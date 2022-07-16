f93736e715d50f2c37f6678a304e3fd311688611
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-16 08:39:11 -0700

    printing double digits wihtout repeatation

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,17 +17,12 @@ for (ch = 48; ch <= 57; ch++)
 		{
 		putchar(ch);
 		putchar(n);
-		putchar(44);
-		putchar(32);
+			if (ch < 56)
+				putchar(44);
+				putchar(32);
 		}
-		/*if (ch != 56 && n != 57)
-		{
-		putchar(44);
-		putchar(32);
-		}
-		if ( n == 57)
-		      break;*/
 	}
+putchar('\n');
 return (0);
 }
 
