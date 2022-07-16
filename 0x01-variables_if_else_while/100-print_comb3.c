7bdf724ec15ef472f38644aea0d7c0b15ea7c981
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-16 08:14:27 -0700

    printing two digits numbers withou reapeating sany

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,20 +11,24 @@ int main(void)
 int ch;
 int n;
 for (ch = 48; ch <= 57; ch++)
-{
 	for (n = 49; n <= 57; n++)
 	{
 		if (n > ch)
 		{
 		putchar(ch);
 		putchar(n);
+		putchar(44);
+		putchar(32);
 		}
-		if (ch != 56 && n != 57)
+		/*if (ch != 56 && n != 57)
 		{
 		putchar(44);
 		putchar(32);
 		}
+		if ( n == 57)
+		      break;*/
 	}
-}
 return (0);
 }
+
+
