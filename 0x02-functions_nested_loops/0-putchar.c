4b0841efebb60a8957a96e4cd6966f14b8fda52b
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-17 16:08:43 -0700

    printing putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
-/** main - entry point
- * desciption: 'this program doesnt use the stdio.h instead its using the main.h from my local device'
+/**
+ * main - entry point
+ * program doesnt use the stdio.h 
+ * instead its using the main.h from my local device'
  *Return: 'exits the code'
 */
 int main(void)
 {
-_putchar("_");
-_putchar("p); 
-_putchar("u");  
-_putchar("t");  
-_putchar("c");  
-_putchar("h");  
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-return(0);
+return (0);
 }
