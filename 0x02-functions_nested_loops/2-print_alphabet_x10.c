864b90ba4c89b076396859d5daa9d6370db4ef58
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-17 17:04:49 -0700

    printing alphabets times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ *print_alphabet_x10 -> prints the aplhabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
+	_putchar(j);
+}
+_putchar('\n');
+}
