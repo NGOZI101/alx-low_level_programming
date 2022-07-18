e3e298c7bf966e1cd37fbc851b1148463d139aca
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-17 17:09:21 -0700

    printing aplhabets times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,9 +5,12 @@
 void print_alphabet_x10(void)
 {
 int j;
-for (j = 'a'; j <= 'z'; j++)
+for (x = 0; x <= 10; x++)
 {
+	for (j = 'a'; j <= 'z'; j++)
+	{     	
 	_putchar(j);
+	}	
 }
 _putchar('\n');
 }
