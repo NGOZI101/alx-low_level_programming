acd3f866259fe6ae0cf21d9228e3a9164d1d66df
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-22 03:47:16 -0700

    checking for aplhabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha -> returns chracters
+ * @c: is variable
+ * Return: returns alpahtes or not alpahabets
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}		
