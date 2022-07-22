ef74935ccea8473de364fa95c9c43fbb1113533d
NGOZI101 <NGOZI101@ngozichukwu.emmanuel.243652@unn.edu.ng>
2022-07-22 03:26:37 -0700

    checking for lower case letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower -> checks if character is lower
+ *
+ *Return:return the lowercase character. return 1 and 0
+ *depending on conditions
+ *c -> variable
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
